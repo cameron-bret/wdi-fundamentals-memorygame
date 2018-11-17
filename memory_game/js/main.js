5bfa2013a78ba4b68a4223575b086acacb358962
C. Bret Nichols <c.bretnichols@cs-macbook-pro.attlocal.net>
2018-11-17 04:28:29 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = [0];
+var cardTwo = [2];
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+console.log("User flipped queen");
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2, cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0] != cardsInPlay[1]) {
+		alert("Sorry try again.")
+	}
