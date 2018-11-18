34ca03f678e84606bde72dbdefe340315c3c5682
C. Bret Nichols <c.bretnichols@cs-macbook-pro.attlocal.net>
2018-11-18 14:09:01 -0500

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,31 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+},
+
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -10,8 +37,10 @@ console.log("Sorry, try again.");
 }
 
 var flipCard = function (cardID) {
-	console.log("User flipped " + cards[cardID])
-	cardsInPlay.push(cards[cardID])
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards.suit);
+	console.log(cards.cardImage);
+	cardsInPlay.push(cards[cardID].rank);
 
 if (cardsInPlay.length === 2, cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
